912253a51b0f18bb609271ad0fb9f8ec3391a18a
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-14 02:14:05 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(string)
+  # tear string into individual words
+  string_array = string.
+  new_string = "" 
+  
+  # reverse each word 
+  string.each do |string_array|
+   
+   #add each reversed word to new string 
+   
+   end 
+   new_string
+  
+end 
