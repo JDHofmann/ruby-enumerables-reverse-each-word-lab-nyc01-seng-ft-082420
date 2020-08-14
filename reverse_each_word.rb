b56ccd30b3e9aa683aee98d6e9a50d40dd2e7e81
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-14 02:19:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,17 @@
 def reverse_each_word(string)
+  
   # tear string into individual words
   string_array = string.split("")
-  new_string = "" 
+  
+  reversed_string_array = []
   
   # reverse each word 
   string.each do |string_array|
-   
-   #add each reversed word to new string 
-   
+    
+   reversed_string_array << string_array.reverse
+
    end 
+   new_string = reversed_string_array.join(" ")
    new_string
   
 end 
